dcca6c4cbd25f60d321f332931a3fc074c6115c3
Paul Broadwith <paul@pauby.com>
2016-07-14 15:57:08 +0100

    Initial commit

---
+++ Public/Printers/Reset-PrinterPort.ps1
@@ -0,0 +1,75 @@
+<#
+# This file is part of the Oxygen distribution (https://github.com/pauby/Oxygen).
+# Copyright (c) 2016 Paul Broadwith
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#>
+
+<#
+.SYNOPSIS
+    Resets the printer port.
+.DESCRIPTION
+    Resets the printer port configuration by removing then adding it with the new one.
+
+    You do not need administrator privileges to use Reset-PrinterPort
+.NOTES
+	Author		: Paul Broadwith (paul@pauby.com)
+	History		: 1.0 - 01/04/16 - Initial version
+.LINK
+    https://www.github.com/pauby/Oxygen
+.PARAMETER PortName
+    Specifies the Name of the port to be reset. You can get this name using Get-PrinterPort.
+.PARAMETER PrinterHostAddress
+    Specifies the IP address of the printer port to be added.
+.OUTPUTS
+	Output is of type [bool] and is true if the port was reset and false otherwise 
+.EXAMPLE
+    Reset-PrinterPort -PortName "HPLJ" -PrinterHostAddress "192.168.10.100"
+
+    Removes the printer port called HPLJ and adds it back again with the same name and IP host address of 192.168.10.100 
+#>
+function Reset-PrinterPort
+{
+    [CmdletBinding()]
+    Param 
+    (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $PortName,
+
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $PrinterHostAddress
+    )
+
+    # check we have a port and remove it
+    if (Test-PrinterPort -PortName $PortName) {
+        Remove-PrinterPort -Name $PortName -ErrorAction SilentlyContinue
+        
+        # check the port is gone
+        if (Test-PrinterPort -PortName $PortName) {
+            $false
+        }
+    }
+
+    # add the port and check it's now been added
+    Add-PrinterPort -Name $PortName -PrinterHostAddress $PrinterHostAddress -ErrorAction SilentlyContinue
+    if (Test-PrinterPort -PortName $PortName) {
+        $true
+    }
+    else {
+        $false
+    }
+}
---
+++ Public/Printers/Test-PrinterPort.ps1
@@ -0,0 +1,52 @@
+<#
+# This file is part of the Oxygen distribution (https://github.com/pauby/Oxygen).
+# Copyright (c) 2016 Paul Broadwith
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#>
+
+<#
+.SYNOPSIS
+    Test-PrinterPort
+.DESCRIPTION
+    Tests whether the printer port specified exists on the computer.
+.NOTES
+	Author		: Paul Broadwith (paul@pauby.com)
+	History		: 1.0.0 - 14/07/16 - Initial version
+.LINK
+    https://www.github.com/pauby/Oxygen
+.PARAMETER PortName
+    Specifies the name of the port to test.
+.OUTPUTS
+	Output is a [bool] and is true if the printer port exists and false otherwise.
+.EXAMPLE
+    Test-PrinterPort -PortName "TestPrinter"
+
+    Tests whether the port named TestPrinter exisst on the computer.
+#>
+function Test-PrinterPort
+{
+    [OutputType([bool])]
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]
+        $PortName
+    )
+
+    if ([bool](Get-PrinterPort | Where { $_.Name -eq $PortName } )) {
+        $true
+    }
+    else {
+        $false
+    }
+}
