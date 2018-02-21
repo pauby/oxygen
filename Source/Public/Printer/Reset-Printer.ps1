e1b1ef5e3e03f7e25882cef28eeaee090ff71a96
Paul Broadwith <paul@pauby.com>
2018-02-21 21:50:40 +0000

    Rewrote function for pipeline and supportsshouldprocess

--- Source/Public/Printer/Reset-Printer.ps1
+++ Source/Public/Printer/Reset-Printer.ps1
@@ -1,12 +1,12 @@
-function Reset-Printer
-{
+function Reset-Printer {
 <#
 .SYNOPSIS
     Reset the specified printer on the local computer.
 .DESCRIPTION
-    Reset the specified printer on the local computer.
+    Reset the specified printer on the local computer by removing the printer,
+    driver and port and adding them back.
 .NOTES
-	Author : Paul Broadwith (https://github.com/pauby)
+    Author : Paul Broadwith (https://github.com/pauby)
 .LINK
     https://www.github.com/pauby/oxygen
 .OUTPUTS
@@ -14,48 +14,66 @@ function Reset-Printer
 .EXAMPLE
     Reset-Printer -Name "HP LaserJet 4" -DriverName "HP LaserJet PS" -PortName "HPLJ4" -PrinterHostAddress "192.168.10.100"
 
-    Resets the printer called "HP LaserJet 4", driver named "HP LaserJet PS",  port "HPLJ4" with IP address 192.168.10.100
+    Resets the printer called "HP LaserJet 4", driver named "HP LaserJet PS", port "HPLJ4" with IP address 192.168.10.100 by removing the port, driver and printer and then adding them back again.
 #>
-
+    [CmdletBinding(SupportsShouldProcess, ConfirmImpact='Medium')]
     [OutputType([System.Boolean])]
     Param
     (
         # Specifies the name of the printer to reset on the local computer.
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateScript( { Test-Printer -Name $_ } )]
         [string]$Name,
 
         # Specifies the name of the driver to reset on the local computer.
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateScript( { Test-PrinterDriver -Name $_ } )]
         [string]$DriverName,
 
         # Specifies the name of the port to reset on the local computer.
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateScript( { Test-PrinterPort -Name $_ } )]
         [string]$PortName,
 
         # Specifies the IP address of the printer to reset on the local computer.
-        [Parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]
-        [string]$PrinterHostAddress
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateScript( { Test-Connection -ComputerName $_ -Count 1 -Quiet } )]
+        [IPAddress]$IPAddress,
+
+        # Force the removal
+        [Switch]$Force
     )
 
-    if (Test-Printer -Name $Name ) {
-        Remove-Printer -Name $Name
-
-        # check the printer is gone
-        if (Test-Printer -Name $Name) {
-            return $false
+    Begin {
+        if (-not $PSBoundParameters.ContainsKey('Confirm')) {
+            $ConfirmPreference = $PSCmdlet.SessionState.PSVariable.GetValue('ConfirmPreference')
+        }
+        if (-not $PSBoundParameters.ContainsKey('WhatIf')) {
+            $WhatIfPreference = $PSCmdlet.SessionState.PSVariable.GetValue('WhatIfPreference')
         }
     }
 
-    # check each of these suceeds.
-    if ((-not (Reset-PrinterPort -PortName $PortName -PrinterHostAddress $PrinterHostAddress)) -or `
-            (-not (Reset-PrinterDriver -DriverName $DriverName)) -or
-            (-not (Add-Printer -Name $Name -DriverName $DriverName -PortName $PortName)) ) {
-        return $false
+    Process {
+        if ($Force -or $PSCmdlet.ShouldProcess("ShouldProcess?")) {
+            Write-Verbose "Removing printer $Name"
+            Remove-Printer -Name $Name
+            Write-Verbose "Checking printer $Name has been removed"
+            if (Test-Printer -Name $Name) {
+                $false
+            }
+            else {
+                # check each of these suceeds.
+                if ((-not (Reset-PrinterPort -PortName $PortName -IPAddress $IPAddress -Force:($Force.IsPresent))) -or
+                        (-not (Reset-PrinterDriver -DriverName $DriverName -Force:($Force.IsPresent))) -or
+                        (-not (Add-Printer -Name $Name -DriverName $DriverName -PortName $PortName -Force:($Force.IsPresent))) ) {
+                    $false
+                }
+                else {
+                    $true
+                }
+            }
+        }
     }
 
-    $true
+    End { }
 }
